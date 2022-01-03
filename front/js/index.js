3bdb0b734efd62a090c1a3baa7f9746029175ede
QuentinGibson <qgibson3@kennesaw.students.edu>
2022-01-03 15:40:27 -0500

    display all product on home page

---
+++ front/js/index.js
@@ -0,0 +1,47 @@
+document.addEventListener("DOMContentLoaded", index)
+
+
+async function index() {
+  const getJSON = new Promise(async function (resolve, reject) {
+    fetch(`http://127.0.0.1:3000/api/products`)
+      .then(response => {
+        if (response.status !== 200) {
+          console.log("There an error loading the products!")
+          reject(console.log(`Status: ${response.status}`))
+        } else {
+          resolve(response.json().catch(console.log()))
+        }
+      })
+  })
+    .then(json => {
+      const itemsElement = document.getElementById('items')
+      json.map(product => {
+        const link = document.createElement('a')
+        const aside = document.createElement("aside")
+        const image = document.createElement("img")
+        const name = document.createElement('h3')
+        const description = document.createElement('p')
+
+        link.id = "product"
+        link.href = `./product.html?id=${product._id}`
+        link.appendChild(aside)
+
+
+        image.src = `${product.imageUrl}`
+        image.alt = `An image of one of our amazing furniture, this piece is named ${product.name}`
+
+        name.classList = "productName"
+        name.innerHTML = `${product.name}`
+
+        description.classList = "productDescription"
+        description.innerHTML = `${product.description}`
+
+        aside.appendChild(image)
+        aside.appendChild(name)
+        aside.appendChild(description)
+
+        itemsElement.appendChild(link)
+      })
+    })
+    .catch("There was an unknown issue while getting the products. Please check the server and try again!")
+}
