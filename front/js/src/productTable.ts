5a3eb47369ba0ebd0777de9b6a32f4239a8392ca
QuentinGibson <qgibson3@students.kennesaw.edu>
2022-02-09 14:41:05 +0000

    add first test (get test)

--- front/js/src/productTable.ts
+++ front/js/src/productTable.ts
@@ -10,13 +10,19 @@ export interface Product {
 
 export class Cart {
   cart: Product[];
-  constructor(items?: Product[]) {
+  constructor(items: Product[] = []) {
     this.cart = [...items];
   }
 
-  update: () => {};
-  find: () => {};
-  all: () => {};
-  get: () => {};
-  remove: () => {};
+  update(id: string, product: Product[]) {}
+  find(id: string) {}
+  all() {}
+  get(id: string) {
+    const index = this.cart.map((product) => product._id).indexOf(id);
+    return this.cart[index];
+  }
+  remove(id: string) {
+    return this.cart.filter((product) => product._id === id);
+  }
+  write: (product: Product[]) => {};
 }
--- front/js/tests/productTable.spec.ts
+++ front/js/tests/productTable.spec.ts
@@ -1,4 +1,5 @@
 import { Cart, Product } from "../src/productTable";
+
 const products: Product[] = [
   {
     colors: ["Blue", "White", "Black"],
@@ -21,85 +22,10 @@ const products: Product[] = [
     altTxt: "Photo of a yellow and black sofa, four seats",
   },
 ];
-test("should be construced", () => {
-  expect(new Cart()).toBeInstanceOf(Cart);
-});
-test("Object structure", () => {
-  expect(new Cart()).toEqual(
-    expect.objectContaining({
-      cart: expect.any(Array),
-      find: expect.any(Function),
-      update: expect.any(Function),
-      remove: expect.any(Function),
-      all: expect.any(Function),
-      get: expect.any(Function),
-    })
-  );
-});
-describe("Product Table:", () => {
-  let store;
-  beforeEach(() => {
-    store = new Cart([products[0], products[1]]);
-  });
-  test("should remove product", () => {
-    const expectedResult = [
-      { _id: "burger", quantity: 1, color: "brown" },
-      { _id: "shake", quantity: 2, color: "pink" },
-    ];
-    const index = 0;
-    store.deleteProduct(index);
-    expect(store.cart).toEqual(expectedResult);
-  });
-  test("should update products", () => {
-    const expectedResult = [
-      { _id: "fries", quantity: 1, color: "yellow" },
-      { _id: "burger", quantity: 1, color: "purple" },
-      { _id: "shake", quantity: 2, color: "pink" },
-    ];
-    const index = 1;
-    const data = { _id: "burger", quantity: 1, color: "purple" };
-    store.updateProduct(index, data);
-    expect(store.cart).toEqual(expectedResult);
-  });
-  describe("should be able to correctly append to the cart", () => {
-    test("by adding new products to the end of the list ", () => {
-      const expectedResult = [
-        { _id: "fries", quantity: 1, color: "yellow" },
-        { _id: "burger", quantity: 1, color: "brown" },
-        { _id: "shake", quantity: 2, color: "pink" },
-        { _id: "onion rings", quantity: 1, color: "brown" },
-      ];
-      store.addProduct({ _id: "onion rings", quantity: 1, color: "brown" });
-      expect(store.cart).toEqual(expectedResult);
-    });
-    test("by ignoring products with no quanitity or color", () => {
-      const expectedResult = [
-        { _id: "fries", quantity: 1, color: "yellow" },
-        { _id: "burger", quantity: 1, color: "brown" },
-        { _id: "shake", quantity: 2, color: "pink" },
-      ];
-      store.addProduct({ _id: "onion rings", quantity: 1, color: "" });
-      store.addProduct({ _id: "fries", quantity: 0, color: "yellow" });
-      expect(store.cart).toEqual(expectedResult);
-    });
-    test("by adding products to the end of list the same id but differnt color", () => {
-      const expectedResult = [
-        { _id: "fries", quantity: 1, color: "yellow" },
-        { _id: "burger", quantity: 1, color: "brown" },
-        { _id: "shake", quantity: 2, color: "pink" },
-        { _id: "burger", quantity: 1, color: "yellow/brown" },
-      ];
-      store.addProduct({ _id: "burger", quantity: 1, color: "yellow/brown" });
-      expect(store.cart).toEqual(expectedResult);
-    });
-    test("by increasing products with the same id and color", () => {
-      const expectedResult = [
-        { _id: "fries", quantity: 3, color: "yellow" },
-        { _id: "burger", quantity: 1, color: "brown" },
-        { _id: "shake", quantity: 2, color: "pink" },
-      ];
-      store.addProduct({ _id: "fries", quantity: 2, color: "yellow" });
-      expect(store.cart).toEqual(expectedResult);
-    });
+
+describe("Product Table: ", () => {
+  test("should get one product from the cart", () => {
+    const cart = new Cart([products[0], products[1]]);
+    expect(cart.get(products[0]._id)).toEqual(products[0]);
   });
 });
